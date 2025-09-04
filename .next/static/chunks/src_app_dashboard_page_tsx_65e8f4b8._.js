2109ca88dc038ae4e3fcec9b280f1fb403ac2882
sswagh777 <74811623+sswagh777@users.noreply.github.com>
2025-09-04 12:39:09 +0530

    Add .next/static/chunks/src_app_dashboard_page_tsx_65e8f4b8._.js 1756969748761

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_65e8f4b8._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_7bff9f4c._.js",
+  "static/chunks/node_modules_226ab86a._.js",
+  "static/chunks/src_0aa1f6f7._.js"
+],
+    source: "dynamic"
+});
