ef6c79e04cde19f54b29687719883e2891f1fe3d
sswagh777 <74811623+sswagh777@users.noreply.github.com>
2025-09-04 16:01:34 +0530

    Add .next/static/chunks/src_app_meter-reading_page_tsx_65e8f4b8._.js 1756981893825

---
+++ .next/static/chunks/src_app_meter-reading_page_tsx_65e8f4b8._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_1049e34c._.js",
+  "static/chunks/src_0d5a8b08._.js"
+],
+    source: "dynamic"
+});
